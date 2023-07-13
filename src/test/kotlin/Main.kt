f57a80a3c94d9e25fbc72fd50c19ea6abde3b1b4
Chris Sinco <csinco@google.com>
2023-07-13 00:27:24 -0700

    Add all icon colors

--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -1,6 +1,5 @@
 package br.com.devsrsouza.svg2compose
 
-import androidx.compose.material.icons.generator.IconWriter
 import java.io.File
 
 fun main() {
@@ -11,6 +10,24 @@ fun main() {
         Pair("FF6C707E", "generalStroke"),
         Pair("FFEBECF0", "generalFill"),
         Pair("FF4682FA", "blueSolid"),
+        Pair("FFEDF3FF", "blueFill"),
+        Pair("FFE7EFFD", "blueFill"), // Old but still in some
+        Pair("FF3574F0", "blueStroke"),
+        Pair("FF55A76A", "greenSolid"),
+        Pair("FFF2FCF3", "greenFill"),
+        Pair("FF208A3C", "greenStroke"),
+        Pair("FFE55765", "redSolid"),
+        Pair("FFFFF7F7", "redFill"),
+        Pair("FFFFF5F5", "redFill"), // Old but still in some
+        Pair("FFDB3B4B", "redStroke"),
+        Pair("FFFFAF0F", "yellowSolid"),
+        Pair("FFFFFAEB", "yellowFill"),
+        Pair("FFC27D04", "yellowStroke"),
+        Pair("FFFFF4EB", "orangeFill"),
+        Pair("FFFCE9D9", "orangeFill"), // Old but still in some
+        Pair("FFE56D17", "orangeStroke"),
+        Pair("FFFAF5FF", "purpleFill"),
+        Pair("FF834DF0", "purpleStroke"),
     )
 
     Svg2Compose.parse(
